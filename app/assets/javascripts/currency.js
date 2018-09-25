686f9684cd84e7fbe20046b5bbdf9356c643b76e
harishspatankar <harish@joshsoftware.com>
2018-09-25 13:39:20 +0530

    ajax call for conversion

--- app/assets/javascripts/currency.js
+++ app/assets/javascripts/currency.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-  $('.to-text-box')
+  $('.from-text-box').on('input', function(){
+    var fromSelected = $('.from-currency option:selected').val();
+    var toSelected = $('.to-currency option:selected').val();
+    urlString = "https://api.exchangeratesapi.io/latest?base=";
+    urlString = urlString + fromSelected;
+    urlString = urlString + "&symbols=";
+    urlString = urlString + fromSelected;
+    urlString = urlString + ",";
+    urlString = urlString + toSelected;
+    $.ajax({url: urlString, success: function(result){
+      var convertedCurrency = parseFloat(result["rates"][""+toSelected]);
+      var toCurrncy = parseFloat($('.from-text-box').val());
+      console.log(convertedCurrency);
+      $('.to-text-box').val(convertedCurrency * toCurrncy);
+      console.log(result);
+    }});
+  });
 });
